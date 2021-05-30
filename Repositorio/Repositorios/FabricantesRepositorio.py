f5309857877401295057aa31840f2ac329a74b4f
hiagojpereira <hiago.pereira01@fatec.sp.gov.br>
2021-05-30 20:39:27 -0300

    Alteração do repositorio de fabricante, inclusão metodos "nomeJaExiste"

--- Repositorio/Repositorios/FabricantesRepositorio.py
+++ Repositorio/Repositorios/FabricantesRepositorio.py
@@ -1,6 +1,7 @@
 from Repositorio.Conexao.conexao import ConexaoPostgre
 from Repositorio.Entidades.Fabricante import Fabricante
 
+
 class FabricantesRepositorio:
     def __init__(self):
         self.conexao = ConexaoPostgre()
@@ -15,7 +16,7 @@ class FabricantesRepositorio:
                       '{fabricante.getNome()}'
                   );"""
 
-        cur.execute(sql)        
+        cur.execute(sql)
         con.commit()
         con.close()
 
@@ -27,12 +28,13 @@ class FabricantesRepositorio:
                          nome
 	              FROM tb_fabricante;"""
 
-        cur.execute(sql)  
+        cur.execute(sql)
         listaFabricanteBanco = cur.fetchall()
-        listaFabricanteEntidade = self.converterListaBancoParaListaEntidade(listaFabricanteBanco)
+        listaFabricanteEntidade = self.converterListaBancoParaListaEntidade(
+            listaFabricanteBanco)
         con.close()
         return listaFabricanteEntidade
-        
+
     def readFabricante(self, id_fabricante):
         con = self.conexao.conectar()
         cur = con.cursor()
@@ -42,17 +44,17 @@ class FabricantesRepositorio:
 	              FROM tb_fabricante
                   WHERE tb_fabricante.id_fabricante = {id_fabricante};"""
 
-        cur.execute(sql)  
-        fabricanteBanco = cur.fetchall()   
+        cur.execute(sql)
+        fabricanteBanco = cur.fetchall()
         con.close()
 
         if len(fabricanteBanco) > 0:
-            fabricanteEntidade = self.converterBancoParaEntidade(fabricanteBanco[0])
+            fabricanteEntidade = self.converterBancoParaEntidade(
+                fabricanteBanco[0])
             return fabricanteEntidade
         else:
-            return None   
+            return None
 
-        
     def updateFabricante(self, fabricante):
         con = self.conexao.conectar()
         cur = con.cursor()
@@ -80,7 +82,7 @@ class FabricantesRepositorio:
         con.commit()
 
         con.close()
-        
+
     def converterBancoParaEntidade(self, fabricanteBanco):
         fabricanteEntidade = Fabricante()
         fabricanteEntidade.setIdFabricante(fabricanteBanco[0])
@@ -90,5 +92,26 @@ class FabricantesRepositorio:
     def converterListaBancoParaListaEntidade(self, listaFabricanteBanco):
         listaFabricanteEntidade = []
         for fabricanteBanco in listaFabricanteBanco:
-            listaFabricanteEntidade.append(self.converterBancoParaEntidade(fabricanteBanco))
+            listaFabricanteEntidade.append(
+                self.converterBancoParaEntidade(fabricanteBanco))
         return listaFabricanteEntidade
+
+    def nomeJaExiste(self, fabricante):
+        con = self.conexao.conectar()
+        cur = con.cursor()
+
+        sql = f"""SELECT count(1)
+	              FROM tb_fabricante
+                  WHERE tb_fabricante.nome = '{fabricante.getNome()}' """
+
+        if fabricante.getIdFabricante() != None:
+            sql += f"AND tb_fabricante.id_fabricante != {fabricante.getIdFabricante() }"
+
+        cur.execute(sql)
+        qtdFabricantes = cur.fetchall()
+        con.close()
+
+        if qtdFabricantes[0][0] > 0:
+            return True
+        else:
+            return False
