d298c01d293139a7f654a1a97590fdbfd38875fa
Otávio Araújo <otavioajardim@gmail.com>
2021-05-30 15:48:15 -0300

    Criação do controlador da nave

---
+++ Controladores/NavesControlador.py
@@ -0,0 +1,32 @@
+from Compartilhados.utilitarios import utilitarios
+from Compartilhados.Excecoes.valoresInvalidosException import ValoresInvalidosException
+from Servicos.NavesServico import NavesServico
+
+
+class NaveControlador:
+    def __init__(self):
+        self.navesServico = NavesServico()
+
+    def createNave(self, nave):
+
+        vlrsObrgNaoPreech = self.navesServico.valoresObrigatoriosNaoPreenchidos(nave)
+        if (len(vlrsObrgNaoPreech) > 0):
+            raise ValoresInvalidosException(menssagem=f"Os valores a seguir são obrigatórios: {utilitarios.listarPorExtenso(vlrsObrgNaoPreech)}. Por favor, preencha-os.")
+
+        nomeJaExiste = self.navesServico.nomeJaExiste(nave)
+        if (nomeJaExiste):
+            raise ValoresInvalidosException(menssagem=f"O nome informado já existe!")
+
+        self.navesServico.createNave(nave)
+
+    def readNaves(self):
+        self.navesServico.readNaves()
+        
+    def readNave(self, id_nave):
+        self.navesServico.readNave(id_nave)
+
+    def updateNave(self, nave):
+        self.navesServico.updateNave(nave)
+
+    def deleteNave(self, id_nave):
+        self.navesServico.deleteNave(id_nave)
