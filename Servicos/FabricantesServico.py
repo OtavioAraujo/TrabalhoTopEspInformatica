c5dd7415e4081d7592643813a0beafdd292d9a59
hiagojpereira <hiago.pereira01@fatec.sp.gov.br>
2021-05-30 20:38:45 -0300

    Criação do servico de fabricante

---
+++ Servicos/FabricantesServico.py
@@ -0,0 +1,45 @@
+from Repositorio.Repositorios.FabricantesRepositorio import FabricantesRepositorio
+
+
+class FabricantesServico:
+
+    def __init__(self):
+        self.fabricantesRepositorio = FabricantesRepositorio()
+
+    def createFabricante(self, fabricante):
+        self.fabricantesRepositorio.createFabricante(fabricante)
+    
+    def readFabricantes(self):
+        return self.fabricantesRepositorio.readFabricantes()
+    
+    def readFabricante(self, id_fabricante):
+        return self.fabricantesRepositorio.readFabricante(id_fabricante)
+
+    def updateFabricante(self, fabricante):
+        self.fabricantesRepositorio.updateFabricante(fabricante)
+
+    def deleteFabricante(self, id_fabricante):
+        self.fabricantesRepositorio.deleteFabricante(id_fabricante)
+
+    def valoresObrigatoriosNaoPreenchidos(self, fabricante):
+        vlrsObrgNaoPreech = []
+
+        if (fabricante.getNome() == None or fabricante.getNome() == ""):
+            vlrsObrgNaoPreech.append(fabricante.getNome())
+
+        return vlrsObrgNaoPreech    
+    
+    def nomeJaExiste(self, fabricante):
+        return self.fabricantesRepositorio.nomeJaExiste(fabricante)
+
+    def idValido(self, idFabricante):
+        try:
+            if idFabricante != None and int(idFabricante):
+                return True
+            else:
+                return False    
+        except:
+            return False
+    
+    def possuiId(self, id_fabricante):
+        return id_fabricante != None
