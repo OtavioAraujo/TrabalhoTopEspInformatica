1718abd537ff35be29d522d35d94ec04f6c33cd0
hiagojpereira <hiago.pereira01@fatec.sp.gov.br>
2021-05-30 14:52:40 -0300

    Entidade Fabricante

---
+++ Repositorio/Entidades/Fabricante.py
@@ -0,0 +1,10 @@
+class Fabricante:
+    def getIdFabricante(self):
+        return self.id_fabricante    
+    def setIdFabricante(self, id_fabricante):
+        self.id_fabricante = id_fabricante
+
+    def getNome(self):
+        return self.nome    
+    def setNome(self, nome):
+        self.nome = nome
