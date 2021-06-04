a99d4bc3068c3d4f14f44ee615d47dc27165b076
Otávio Araújo <otavioajardim@gmail.com>
2021-06-04 08:03:34 -0300

    Criação do serviço do usuário

---
+++ Servicos/UsuariosServico.py
@@ -0,0 +1,51 @@
+from Repositorio.Repositorios.UsuariosRepositorio import UsuariosRepositorio
+
+
+class UsuariosServico:
+
+    def __init__(self):
+        self.usuariosRepositorio = UsuariosRepositorio()
+
+    def createUsuario(self, usuario):
+        self.usuariosRepositorio.createUsuario(usuario)
+    
+    def readUsuarios(self):
+        return self.usuariosRepositorio.readUsuarios()
+    
+    def readUsuario(self, id_usuario):
+        return self.usuariosRepositorio.readUsuario(id_usuario)
+
+    def updateUsuario(self, usuario):
+        self.usuariosRepositorio.updateUsuario(usuario)
+
+    def deleteUsuario(self, id_usuario):
+        self.usuariosRepositorio.deleteUsuario(id_usuario)
+
+    def valoresObrigatoriosNaoPreenchidos(self, usuario):
+        vlrsObrgNaoPreech = []
+
+        if (usuario.getNome() == None or usuario.getNome() == ""):
+            vlrsObrgNaoPreech.append(usuario.getNome())
+
+        if (usuario.getSenha() == None or usuario.getSenha() == ""):
+            vlrsObrgNaoPreech.append(usuario.getSenha())
+
+        if (usuario.getEMail() == None or usuario.getEMail() == ""):
+            vlrsObrgNaoPreech.append(usuario.getEMail())
+
+        return vlrsObrgNaoPreech    
+    
+    def emailJaExiste(self, usuario):
+        return self.usuariosRepositorio.emailJaExiste(usuario)
+
+    def idValido(self, idUsuario):
+        try:
+            if idUsuario != None and int(idUsuario):
+                return True
+            else:
+                return False    
+        except:
+            return False
+    
+    def possuiId(self, id_usuario):
+        return id_usuario != None
