8deabfe424731488ef419714ed034728bc23c383
Otávio Araújo <otavioajardim@gmail.com>
2021-06-06 12:04:04 -0300

    Criação da classe de estilização da view

---
+++ Views/Compartilhados/Estilos.py
@@ -0,0 +1,9 @@
+class Estilos:
+    
+    def atributoChave(self, texto):
+        return f'\033[1;32m{texto}\033[m'
+    
+    def atributoValor(self, texto):
+        return f'\033[1;30m{texto}\033[m'
+
+    
