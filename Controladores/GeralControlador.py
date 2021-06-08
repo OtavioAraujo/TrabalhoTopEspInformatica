da754067b7c5b6c9043f9cde4672e4bcee19509c
Otávio Araújo <otavioajardim@gmail.com>
2021-06-07 23:17:54 -0300

    Criação do controlador geral

---
+++ Controladores/GeralControlador.py
@@ -0,0 +1,37 @@
+import os
+import json
+from Servicos.NavesServico import NavesServico
+from Servicos.UsuariosServico import UsuariosServico
+from Servicos.FabricantesServico import FabricantesServico
+import zipfile as zip
+
+
+class GeralControlador:
+    def __init__(self):
+        self.fabricantesServico = FabricantesServico() 
+        self.navesServico = NavesServico()        
+        self.usuariosServico = UsuariosServico()
+    
+    def exportarTodosOsDados(self):
+        fabricantes = self.fabricantesServico.readFabricantes()
+        naves = self.navesServico.readNaves()
+        usuarios = self.usuariosServico.readUsuarios()
+
+        for nave in naves:
+            nave.capacidade_carga = float(nave.capacidade_carga) if nave.capacidade_carga != None else None
+            nave.preco = float(nave.preco) if nave.preco != None else None
+
+        listaTotal = {
+            "fabricantes" : [fabricante.__dict__ for fabricante in fabricantes],
+            "naves": [nave.__dict__ for nave in naves],
+            "usuarios" : [usuario.__dict__ for usuario in usuarios],
+        }
+        listaTotalJson = json.dumps(listaTotal, ensure_ascii=False)
+
+        dirDownloads = os.path.join(os.sep, os.path.expanduser('~') , 'Downloads', )
+        dir = os.path.join(os.sep, dirDownloads, 'DadosExportadosTopEspInf.zip')
+        
+        arquivoZip = zip.ZipFile(dir, 'w')
+        arquivoZip.writestr('DadosExportadosTopEspInf.json', listaTotalJson)
+        arquivoZip.close()
+        
